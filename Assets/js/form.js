39ce7cc26c014c6e2e258a2233eff2aeeb7573b4
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-05-03 13:10:54 -0700

    'no data' warning prompt added

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,11 +1,14 @@
-const userName = document.querySelector('#userName');
+const userName = document.querySelector('#bloggerName');
 const entryTitle = document.querySelector('#entryTitle');
 const entryContent = document.querySelector('#entryContent');
 const submitButton = document.querySelector('#submitButton');
 
 //get existing blog data
 const blogArchiveArray = new Array;
-const blogArchive = JSON.parse(localStorage.getItem('blogArchive'));
+if(localStorage.getItem('blogArchive')) {
+    blogArchiveArray.push(JSON.parse(localStorage.getItem('blogArchive')));
+}
+
 
 function submitNewBlog (event) {
     event.stopPropagation();
@@ -15,6 +18,11 @@ function submitNewBlog (event) {
         title: entryTitle.value,
         content: entryContent.value
     }
+
+    //ensure completion of the form
+    if (userName.value === "" || entryTitle.value === "" || entryContent.value === "") {
+        alert('All fields must be completed before submitting your blog entry.');
+    } else {
     //add new data to the existing blog data
     blogArchiveArray.push(newEntryData);
 
@@ -22,6 +30,7 @@ function submitNewBlog (event) {
 
     //redirect to blog.html
     window.location.href = "blog.html";
+    }
 }
 
 submitButton.addEventListener ('click', submitNewBlog);
